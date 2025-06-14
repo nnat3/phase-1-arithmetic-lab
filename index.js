b9e566ae75b7c052cf0169cdd8d5f14f05586956
Neathan Kungu <natekungu857@gmail.com>
2025-06-14 10:11:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-let multiply = (num1, num2) => {
-    let num1 = 31;
-    let num2 = 2;
-    return num1 * num2;
-}
+const num1 = 31
+const num2 = 2
+const multiply = 31 * 2
+const random = Math.floor(Math.random() * 100) + 1
+const num3 = 16
+const num4 = 6
+const mod = (num3 % num4)
+const max = Math.max(5,10, 15, 20)
